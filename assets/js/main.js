29394fd0631526eece05142d58400321c74dcde8
Manthan Bhatt <manthan@markettime.com>
2021-12-18 15:13:41 +0530

    added UI for add guest, date picker and room info modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,17 @@ document.addEventListener("DOMContentLoaded", () => {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     })
 });
+
+$(function () {
+    $('#checkIn').daterangepicker({
+        opens: 'left'
+    }, function (start, end, label) {
+        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+
+    $('#checkOut').daterangepicker({
+        opens: 'left'
+    }, function (start, end, label) {
+        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+});
