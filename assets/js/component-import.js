5e438c326fb3669f553263c9e3a35fcd32a49d00
Manthan Bhatt <manthan@markettime.com>
2021-12-20 15:49:32 +0530

    component base html

---
+++ assets/js/component-import.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    $("#header").load("component/header.html");
+    $("#popup-modal").load("component/room-detail.html");
+    $("room-card").load("component/room-card.html");
+});
